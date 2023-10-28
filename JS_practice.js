002fad78ed2da7cef6a85d5f61ccfa935caa958e
sha-hul <85384309+sha-hul@users.noreply.github.com>
2023-10-28 13:12:45 +0530

    commit 28 oct

--- JS_practice.js
+++ JS_practice.js
@@ -120,6 +120,138 @@
   
  //arrow function
 
- const checkEven=(a,b)=> a%2===0 && b%2===0;
- console.log(checkEven(1,2));
- console.log(checkEven(2,Math.ceil(1.4)));
+//  const checkEven=(a,b)=> a%2===0 && b%2===0;
+//  console.log(checkEven(1,2));
+//  console.log(checkEven(2,Math.ceil(1.4)));
+
+//Scope 
+
+//It defines the accessibility of variable
+
+//global scope
+
+// var Id= 23; //I can use any where in this file
+
+// //function scope
+// function nameEUUK() {
+//     const countryIDEUUK=[1,2,3,8,23]
+//     var countryIdEUUK=[1,2,3,8,23]
+//     console.log(countryIDEUUK)
+//     console.log(Id)
+//  //block scope   
+//     if(-Infinity){
+//         const countryName={'23':'Swedan'}
+//         var counName={'23':'Swedan'}
+//     }
+//     console.log(counName) //do not use var in case of block scope
+// }
+// nameEUUK();
+//function scope
+//console.log(countryIDEUUK);//error it is not defined
+//console.log(countryIdEUUK)//error it is not defined
+//block scoe 
+//console.log(counName) //error bcoz we can't use any function variable out
+//console.log(countryName) // error  it is not defined
+
+//Lexical scope
+
+// if ('go') {
+//     const outer=10;
+//     if ('go inner') {
+//         const inner =20;
+//         console.log(outer);
+//         console.log(inner);
+//     }
+// }
+
+//Hoisting
+
+//The variable and func declarations are moved to the top of scope before code execution.
+
+// console.log(a) //undefined
+
+// var a=20;
+
+// add(a,30,40,10) //output: 100 (while func declaration entirely hoisted.)
+
+// function add(a,...params) {
+//     Number(a)
+//     let temp=0;
+//     params.forEach(value=>{
+//         temp+=value;
+//     })
+//     console.log(a+temp)
+// }
+
+// console.log(abc+" "+haha);
+// var abc= function () {
+//     console.log('hello')
+// }
+
+// var haha=()=>console.log('hey man!!!')
+// haha()
+// abc()
+
+//-----Call back function------
+
+//It is function passed as an argument to the another function.
+
+const carRepair=(a,functoExecute)=>{
+    console.log('Working on car repair');
+    //if work is done call the owner
+    functoExecute(a)
+}
+
+const callOwner=(a)=>{
+    if (a==true) {
+        console.log('Work Done, calling the owner')
+    } else {
+        console.log('Work is not done, call the owner tommorrow')
+    }
+}
+
+//callback function
+carRepair(false,callOwner);
+
+//-----closure---
+
+//Here's a brief example:
+
+function outerFunction() {
+  let outerVariable = "I am from outer function";
+
+  function innerFunction() {
+    console.log(outerVariable);
+  }
+
+  return innerFunction;
+}
+
+// Create a closure by calling outerFunction and assigning the result to a variable
+let closureExample = outerFunction();
+
+// Invoke the inner function, which still has access to outerVariable
+closureExample(); // Outputs: "I am from outer function"
+
+
+//In the above example, innerFunction is defined inside outerFunction, and it has access to the
+//  outerVariable of its containing scope.When we call outerFunction, it returns innerFunction,
+//  creating a closure.Even though outerFunction has finished executing, the inner function still
+//  "remembers" and has access to the outerVariable.
+
+console.log('----------array.sort()===========')
+let numbers = [5, 2, 8, 1, 4];
+
+numbers.sort(function(a, b) {
+  console.log(a+"-"+b+":"+(a-b) ); 
+});
+numbers.sort((a,b)=>a-b) // ascending order
+console.log(numbers); // Outputs: [1, 2, 4, 5, 8]
+numbers.sort((a,b)=>b-a) //descending order
+console.log(numbers); // Outputs: [ 8, 5, 4, 2, 1 ]
+let words = ['Shahul', 'Anom','anom', 'Head', 'Maxwell', 'Ravindra'];
+
+words.sort();// ascending order
+console.log(words)
+words.sort((a,b)=> b.localeCompare(a)) //descending order
+console.log(words)
